87073e1769191d460dc78d0feb5e4d3122f32b52
Hu yiqian <huyq2002@qq.com>
2021-07-30 15:19:07 +0800

    Update UrlConst.java

--- app/src/main/java/com/nsb/xmatrix/utils/UrlConst.java
+++ app/src/main/java/com/nsb/xmatrix/utils/UrlConst.java
@@ -4,7 +4,7 @@ import com.nsb.xmatrix.MyApp;
 
 public class UrlConst {
     //Todo: replace the URL with your own Flask restful API backend
-    public static String baseUrl = MyApp.isDebug() ? "http://192.168.3.13:5000" : "http://http://47.100.80.164";
+    public static String baseUrl = MyApp.isDebug() ? "http://192.168.3.13:5000" : "http://47.100.80.164";
     // farm_land API URL
     public static String upload =  baseUrl + "/upload";
     // harvest predict API URL
